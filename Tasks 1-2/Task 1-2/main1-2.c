2a9fabd7deb880a7e603b62d85e0a17a4943d9f9
BulaniiSergey <91825050+BylaniiSergey@users.noreply.github.com>
2023-10-10 18:28:37 +0300

    Переделал задание 1-2

--- Tasks 1-2/Task 1-2/main1-2.c
+++ Tasks 1-2/Task 1-2/main1-2.c
@@ -2,29 +2,46 @@
 #include <math.h>
 
 
-/*
+/**
 * @brief Перевод грамм в унции
 * @return Возвращает унции
-* */
-double gram_to_ounces( );
+*/
+double gram_to_ounces(double gram);
 
 
-/*
+/**
+* @brief Получает вещественное число
+* @return Возвращает вещественное число
+*/
+double getscanf_s(double gram);
+
+
+/**
 * @brief Точка входа в программу
 * @return Возвращает 0, если программа работает корректно, иначе не 0
-* */
+*/
 int main()
 {
 	double gram;
-	scanf_s("%lf", &gram);
+	int res = scanf_s("%lf", &gram);
+	getscanf_s(res);
 	double ounces = gram_to_ounces(gram);
-	printf("%lf\n", ounces);
+	printf("%lf", ounces);
 	return 0;
+}
+
+
+double getscanf_s(double gram)
+{
+	if (gram != 1)
+	{
+		printf("%lf", "Введено неверное значение");
+		abort();
+	}
 }
 
 
 double gram_to_ounces(double gram)
 {
-	double ounces = gram * 28.3;
-	return ounces;
+	return gram * 28.3;
 }
