23d9c166da3c69c81483e3313ff59d6e5997aee5
BulaniiSergey <91825050+BylaniiSergey@users.noreply.github.com>
2023-11-01 14:20:05 +0300

    Переделал задание 3-1

--- Tasks 3-1/Task 3-1/main 3-1.c
+++ Tasks 3-1/Task 3-1/main 3-1.c
@@ -1,59 +1,54 @@
 #include <stdio.h>
-#include <locale.h>
-#include <errno.h>
-#include <stdbool.h>
-#include <math.h>
-#include <float.h>
 #include <stdlib.h>
-
+#include <math.h>
+#include <stdbool.h>
+#include <errno.h>
+#include <float.h>
 /**
-* @brief Функция для проверки Y на существование.
-* @return Возвращает true, если Y существует, иначе false.
+ * @brief Функция проверяющая существование функции в этой точке.
+ * @param x Входной параметр - точка x.
+ * @return Возвращает результат.
+ */
+double result(double x);
+/**
+* @brief Функция принимающая и проверяющая значение на ввод.
+* @param message - текст сообщения для пользователя.
+* @return Значение.
 */
-bool is_exist(double x);
+double get_value(const char* message);
 
 /**
-* @brief Функция для подсчёта Y.
-* @return Y.
-*/
-double get_Y(double x);
-
-/**
-* @brief Получает целое значение
-* @remarks Экстренное завершение программы, в случае неправильного ввода
-* @return Целое значение
+* @brief Точка входа в программу
+* @return Возврящает 0, если программа работает верно, иначе 1
 */
 int main()
 {
-    const double x_start = 1.0;
-    const double x_finish = 2.0;
-    const double step = 0.1;
+    const double x_start = get_value("Start for x: ");
     double x = x_start;
-
-    while (x <= x_finish)
+    const double x_finish = get_value("Finish for x: ");
+    const double dx = get_value("Step for x: ");
+    while ((x - x_finish) <= DBL_MIN)
     {
-        if (is_exist(x))
-        {
-            double y = get_Y(x);
-            printf_s("%lf %lf\n", x, y);
-        }
-        else
-        {
-            printf_s("%lf Нет решения\n", x);
-
-        }
-        x += step;
+        double y = result(x);
+        printf("%lf\t%0.2lf\t\n", x, y);
+        x += dx;
     }
-
-    return EXIT_SUCCESS;
+    return 0;
 }
-
-bool is_exist(double x)
-{
-    return (abs(x)>= DBL_MIN);
-}
-
-double get_Y(double x)
+double result(double x)
 {
     return ((cos(2 / x)) - 2 * sin(1 / x) + (1 / x));
+}
+double get_value(const char* message)
+{
+    double a;
+    printf("%s", message);
+    int res = scanf_s("%lf", &a);
+    if (res != 1)
+    {
+        errno = EIO;
+        perror("Wrong value");
+        abort();
+    }
+    return a;
 }
