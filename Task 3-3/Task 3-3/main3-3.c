d251a59b9915ee448e6aaf28f4dfdb0ab8ed1b0f
BulaniiSergey <91825050+BylaniiSergey@users.noreply.github.com>
2023-11-28 12:33:15 +0300

    Переделал задание 3-3

--- Task 3-3/Task 3-3/main3-3.c
+++ Task 3-3/Task 3-3/main3-3.c
@@ -3,6 +3,7 @@
 #include <math.h>
 #include <float.h>
 #include <errno.h>
+#include <locale.h>
 
 /**
 * @brief Функция принимающая и проверяющая значение на ввод
@@ -12,11 +13,26 @@
 double get_step(const char* message);
 
 /**
+* @brief Функция принимающая и проверяющая значение на ввод
+* @param message - текст сообщения для пользователя
+* @return Значение
+*/
+double get_value(const char* message);
+
+/**
  * @brief Функция возвращающая значение функции в данной точке
  * @param x - точка
  * @return Значение функции в точке x
  */
 double function(double x);
+
+/**
+* @brief Функция для s_i
+* @param i -  перменная
+* @param x -  точка
+* @return s_i
+*/
+double get_s_i(double i, double x);
 
 /**
  * @brief Функция возвращающая значение суммы ряда в этой точке.
@@ -31,12 +47,13 @@ double summ(double x, double e);
 */
 int main()
 {
-	double const a = 0.1;
+	setlocale(LC_ALL, "RU");
+	double a = get_value("Введите(a) : ");
 	double x = a;
-	double const b = 0.8;
+	double b = get_value("Введите(b) : ");
 	double step = get_step("Enter step: ");
 	double e = pow(30, -5);
-	for (double i = a; i - b <= DBL_MIN; i += step)
+	for (double i = a; i - b <= DBL_EPSILON; i += step)
 	{
 		printf("%lf\t%lf\t%lf\t\n", i, function(i), summ(i, e));
 	}
@@ -45,19 +62,21 @@ int main()
 
 double function(double x)
 {
-	return (1/4)*log((1+x)/(1-x))+(1/2)*atan(x);
+	return (1 / 4) * log((1 + x) / (1 - x)) + (1 / 2) * atan(x);
 }
 
-double summ(double x, double e)
+double get_s_i(double i, double x)
+{
+	return (4 * i * pow(x, 4) + pow(x, 4)) / (4 * i + 5);
+}
+
+double summ(double x)
 {
 	double sum = 0;
-	int i = 1;
-	double s_i = (4*i*pow(x,4)+pow(x,4))/(4*i+5);
-	sum += s_i;
-	for (int i = 2; s_i > e; i++)
+	sum += 0;
+	for (int i = 0; function(x) - sum <= DBL_EPSILON; i++)
 	{
-		s_i += (4 * i * pow(x, 4) + pow(x, 4)) / (4 * i + 5);
-		sum += s_i;
+		sum += get_s_i(i, x);
 	}
 	return sum;
 }
@@ -74,4 +93,28 @@ double get_step(const char* message)
 		abort();
 	}
 	return a;
+}
+
+double get_value(const char* message)
+{
+	double b;
+	printf("%s", message);
+	int res = scanf_s("%lf", &b);
+	if (res != 1 || b <= 0)
+	{
+		errno = EIO;
+		perror("Wrong value");
+		abort();
+	}
+	return b;
+}
+
+void check_range(double a, double b)
+{
+	if (a > b)
+	{
+		errno = EIO;
+		perror("ERROR");
+		abort();
+	}
 }
