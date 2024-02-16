694a9de3f02af9400ff7dbe25bd29acdc7f4e973
BulaniiSergey <91825050+BylaniiSergey@users.noreply.github.com>
2024-02-16 16:26:22 +0300

    Решатель квадратного уравнения

---
+++ 2 симестр/Equation/Solver.Tests/Solver.Tests.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace SolverTests
+{
+	TEST_CLASS(SolverTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
